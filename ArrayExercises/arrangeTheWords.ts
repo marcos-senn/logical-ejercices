10183a5a942fd6c9cfdb823d49941bfc4190aea6
Marcos Antonio Senn <marcossenn@gmail.com>
2024-03-12 15:15:15 -0300

    coments in arrangeTheWords

--- ArrayExercises/arrangeTheWords.ts
+++ ArrayExercises/arrangeTheWords.ts
@@ -8,18 +8,19 @@ Output: "In the are lines order printed reverse."
 
 */
 
+//We are going to simulate the input
 let input: String = "The lines are printed in reverse order.";
 let transform = (input: String): String => {
-    let splittedInput: String[] = input.split(" ");
-    let sortedInput: String[] = splittedInput
-        .sort((a, b) => a.length - b.length)
-        .map((element) => element.toLowerCase().replace(".", ""));
+    let splittedInput: String[] = input.split(" "); //We transform the input into an array
+    let sortedInput: String[] = splittedInput //We sort the array by length
+        .sort((a, b) => a.length - b.length) 
+        .map((element) => element.toLowerCase().replace(".", "")); //We transform the array to lowercase and remove the period
     sortedInput[0] =
-        sortedInput[0].charAt(0).toLocaleUpperCase() + sortedInput[0].slice(1);
+        sortedInput[0].charAt(0).toLocaleUpperCase() + sortedInput[0].slice(1); //We capitalize the first letter of the first word
     sortedInput[sortedInput.length - 1] =
-        sortedInput[sortedInput.length - 1] + ".";
-    let output: String = sortedInput.join(" ");
-    return output;
+        sortedInput[sortedInput.length - 1] + "."; //We add the period to the last word
+    let output: String = sortedInput.join(" "); //We join the array into a string
+    return output; //return the final string
 };
 
 transform(input);
